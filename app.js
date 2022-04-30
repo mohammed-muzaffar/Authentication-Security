576dbc695b401ca2c65eeeb048236452e90b5f62
Mohammed Muzaffar <mohammedmuzaffar294@gmail.com>
2022-04-30 22:55:22 +0530

    Level 4 security, Hash and Salting

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+// const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 11;
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
@@ -34,37 +38,44 @@ app.get("/login", function(req, res) {
 });
 
 
-app.post("/register", function(req, res){
+app.post("/register", function(req, res) {
 
-  const newUser = new User({
-    email: req.body.username,
-    password: md5(req.body.password)
-  });
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
 
-  newUser.save(function(err){
-    if(err){
-      console.log(err);
-    } else {
-      res.render("secrets");
-    }
-  });
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
+
+    newUser.save(function(err) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.render("secrets");
+      }
+    });
+  })
 });
 
-app.post("/login", function(req, res){
+app.post("/login", function(req, res) {
 
-const username = req.body.username;
-const password = md5(req.body.password);
+  const username = req.body.username;
+  const password = req.body.password;
 
-  User.findOne({email: username}, function(err, foundUser){
-    if (err){
-      console.log(err);
-    } else {
-      if (foundUser){
-        if(foundUser.password === password) {
-          res.render("secrets");
+  User.findOne({
+      email: username
+    }, function(err, foundUser) {
+      if (err) {
+        console.log(err);
+      } else {
+        if (foundUser) {
+          bcrypt.compare(password, foundUser.password, function(err, passwordMatch) {
+            if (passwordMatch === true){
+              res.render("secrets");
+            }
+          });
         }
       }
-    }
   });
 });
 
@@ -74,6 +85,6 @@ const password = md5(req.body.password);
 
 
 
-app.listen(3000, function(){
+app.listen(3000, function() {
   console.log("Server is running successfully on port 3000");
 });
